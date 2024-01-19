106ad6b0e547ea95d031e3f99d761adacc552b5a
Vegadelalyra <lyradeperseo@gmail.com>
2024-01-19 13:36:25 -0500

    feat: nuxt 2 + vuetify sign in webapp, vuetify options not working

---
+++ README.md
@@ -0,0 +1,47 @@
+# nuxt starter
+
+## How to start a nuxt v2 brand new projext?
+
+```bash
+# Run the Nuxt Starter Templates Community CLI
+$ npx nuxi init [-t --template template] [project-name] 
+
+# e.g. Get started with a minimal Nuxt v2 template
+$ npx nuxi init -t v2 my-nuxt2-app
+
+```
+
+## How to implement Vuetify ? 
+
+Once you have created your Nuxt project, move to the project's folder and run
+```bash
+$ vue add vuetify
+```
+
+### ...and that's not all! 
+
+
+
+
+# _______________________________________
+
+Below, default README Nuxt 2 stuff.
+
+## Build Setup
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+```
+
+For detailed explanation on how things work, check out [documentation](https://nuxtjs.org).
---
+++ pages/sign-in/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app>
+    <div class="container">
+        <v-form v-model="valid" class="login">
+            <h1>Sign In</h1>
+            <v-text-field label="User Name:" type="email" counter="50" :rules="[required('name')]" />
+            <v-text-field type="password" label="Password" counter="50" 
+                :rules="[required('password'), minLength('password', 5)]" />
+            <v-btn color="primary" :disabled="!valid">Submit</v-btn>
+        </v-form>
+    </div>
+  </v-app>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            valid: false,
+            required(propertyType) {
+                return v => 
+                    (v && v.length > 0) || `You must input a ${propertyType}`
+            },
+            minLength(propertyType, minLength) {
+                return v => (v && v.length >= minLength) ||
+                   `${propertyType} must be less than ${minLength} characters.`
+            }
+        }
+    }
+};
+</script>
+
+<style lang="sass" scoped>
+.container
+    max-width: 1140px
+    h1
+      font-size: 50px
+    .login
+        display: flex
+        flex-direction: column
+        justify-content: center
+        width: 50%
+        margin: 40px auto
+        h1
+            text-align: center
+            padding: 50px
+        button
+            width: 200px
+            margin: 0 auto
+            margin-top: 20px
+</style>
